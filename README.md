cdacfec890d935addf950312917126560491bf5a
PeakLinux <linuxpeak01@gmail.com>
2022-08-07 19:45:28 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+### Main Repositories for the Ascent Package Manager
+
+genRepo.py is for generating your own repositories. To get started generating your own repositories, first run the following code block to set up the dependencies for genRepo
+```sh
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+After installing all required dependencies, run `python genRepo.py <path to repo-dev.json> <path to repo-pub.json>` and let the script take care of the rest
+
+- The structure of a repo-dev.json file is as follows:
+```json
+{
+	"packages": {
+		"pkgname": {
+			"vcs": "<vcs (currently only git is supported)>",
+			"repo": "<url to source repository>",
+			"releaseMethod": "<Type of releases to use (currently only tags are implemented)>",
+			"buildSystem": "<buildsystem to use (currently only default (./configure, make, make install) is implemented>"
+		}
+	}
+}
+```
---
+++ genRepo.py
@@ -0,0 +1,154 @@
+import git
+from rich import console, progress
+import sys
+import os
+import shutil
+import json
+
+# Credit: https://stackoverflow.com/questions/51045540/python-progress-bar-for-git-clone
+
+
+class Progressbar(git.RemoteProgress):
+	OP_CODES = [
+		"BEGIN",
+		"CHECKING_OUT",
+		"COMPRESSING",
+		"COUNTING",
+		"END",
+		"FINDING_SOURCES",
+		"RECEIVING",
+		"RESOLVING",
+		"WRITING",
+	]
+	OP_CODE_MAP = {
+		getattr(git.RemoteProgress, _op_code): _op_code for _op_code in OP_CODES
+	}
+
+	def __init__(self) -> None:
+		super().__init__()
+		self.progressbar = progress.Progress(
+			progress.SpinnerColumn(),
+			# *progress.Progress.get_default_columns(),
+			progress.TextColumn("[progress.description]{task.description}"),
+			progress.BarColumn(),
+			progress.TextColumn(
+				"[progress.percentage]{task.percentage:>3.0f}%"),
+			"eta",
+			progress.TimeRemainingColumn(),
+			progress.TextColumn("{task.fields[message]}"),
+			console=console.Console(),
+			transient=False,
+		)
+		self.progressbar.start()
+		self.active_task = None
+
+	def __del__(self) -> None:
+		# logger.info("Destroying bar...")
+		self.progressbar.stop()
+
+	@classmethod
+	def get_curr_op(cls, op_code: int) -> str:
+		"""Get OP name from OP code."""
+		# Remove BEGIN- and END-flag and get op name
+		op_code_masked = op_code & cls.OP_MASK
+		return cls.OP_CODE_MAP.get(op_code_masked, "?").title()
+
+	def update(
+		self,
+		op_code: int,
+		cur_count: str | float,
+		max_count: str | float | None = None,
+		message: str | None = "",
+	) -> None:
+		# Start new bar on each BEGIN-flag
+		if op_code & self.BEGIN:
+			self.curr_op = self.get_curr_op(op_code)
+			# logger.info("Next: %s", self.curr_op)
+			self.active_task = self.progressbar.add_task(
+				description=self.curr_op,
+				total=max_count,
+				message=message,
+			)
+
+		self.progressbar.update(
+			task_id=self.active_task,
+			completed=cur_count,
+			message=message,
+		)
+
+		# End progress monitoring on each END-flag
+		if op_code & self.END:
+			# logger.info("Done: %s", self.curr_op)
+			self.progressbar.update(
+				task_id=self.active_task,
+				message=f"[bright_black]{message}",
+			)
+
+
+class cloneRepo:
+	def clone(url):
+		package = url.split('/')[-1]
+
+		if os.path.isdir(package) and len(os.listdir(package)) != 0:
+			answer = input(
+				f"Repository {package} already exists. Would you like to remove the directory? (Y/n) ")
+			if answer == 'Y' or answer == 'y' or answer == '':
+				shutil.rmtree(package)
+			elif answer == 'n':
+				return
+
+		print(f"Cloning {package}")
+
+		git.Repo.clone_from(
+			url=url,
+			to_path=url.split('/')[-1],
+			progress=Progressbar()
+		)
+
+
+class readJsonFile:
+	def getPackageNames(jsonfile):
+		with open(jsonfile) as f:
+			data = json.load(f)
+			packages = data['packages']
+
+			return packages
+
+	def getTags(devFile, pubFile):
+		packages = readJsonFile.getPackageNames(devFile)
+		packages2 = {}
+		for package in packages:
+			tagsPreSplit = []
+			tags = []
+			cloneRepo.clone(packages[package]['repo'])
+			with open(f'{package}/.git/packed-refs', 'r') as f:
+				lines = f.readlines()
+
+				for line in lines:
+					line = line.strip()
+					line = line.split(' ')
+
+					if len(line) < 2:
+						continue
+					else:
+						tagsPreSplit.append(line[1])
+
+			for line in tagsPreSplit[2:]:
+				line = line.split('/')
+				tags.append(line[2])
+			packages2[package] = {
+				"vcs": packages[package]['vcs'],
+				"repo": packages[package]['repo'],
+				"versions": tags,
+				"tarball": f"{packages[package]['repo']}/tarball/refs/tags/%%release%%"
+			}
+
+		pkg = {
+			"packages": packages2
+		}
+		
+		with open(pubFile, "w") as f:
+			f.write(json.dumps(pkg, indent=4))
+
+
+readJsonFile.getTags('Utilities/repo-dev.json', 'Utilities/repo-pub.json')
